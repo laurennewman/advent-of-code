bd62946706a305804d53955cd8a1d74c4219b10f
Lauren Newman <lanewman@redhat.com>
2020-12-06 12:22:48 -0500

    begin day 2

---
+++ 2020/02/README.md
@@ -0,0 +1,11 @@
+# [Advent of Code 2020 - Day 2](https://adventofcode.com/2020/day/2)
+
+## Part 1
+How many passwords are valid?
+
+Solution:
+
+## Part 2
+
+
+Solution:
---
+++ 2020/02/password-philosophy.py
@@ -0,0 +1,16 @@
+
+def password_philosophy():
+    with open('input') as f:
+        report = f.read().splitlines()
+
+
+def solve_part_one():
+    pass
+
+
+def solve_part_two():
+    pass
+
+
+if __name__ == "__main__":
+    password_philosophy()
