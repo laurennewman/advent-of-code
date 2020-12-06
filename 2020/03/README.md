bc8860f7d567f3a4bbc8c463dd603d584759f86a
Lauren Newman <lanewman@redhat.com>
2020-12-06 17:10:58 -0500

    solved day 3 part 2

--- 2020/03/README.md
+++ 2020/03/README.md
@@ -6,6 +6,6 @@ How many trees would you encounter?
 Solution: 276
 
 ## Part 2
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
 
-
-Solution:
+Solution: 7812180000
--- 2020/03/toboggan-trajectory.py
+++ 2020/03/toboggan-trajectory.py
@@ -3,20 +3,24 @@ def toboggan_trajectory():
     with open('input') as f:
         map_section = f.read().splitlines()
 
-    expanded_map = create_map(map_section)
+    slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    expanded_map = create_map(map_section, max([moves[0] for moves in slopes]))
 
-    trees_encountered_part_one = solve_part_one(expanded_map)
+    for slope in slopes:
+        encounter_trees(expanded_map, slope)
 
-    print(f'Part 1: {trees_encountered_part_one} trees')
-    # print(f'Part 2: {len(valid_passwords_part_two)} trees')
+    print(f'Part 1: {slopes[1][2]} trees')
+
+    product_of_trees_encountered = multiply_trees(slopes)
+    print(f'Part 2: {product_of_trees_encountered} trees')
 
 
-def create_map(map_section):
+def create_map(map_section, moves_to_the_right):
     rows = len(map_section)
     cols = len(map_section[0])
-    cols_needed = (rows - 1) * 3
+    cols_needed = (rows - 1) * moves_to_the_right
     multiplier = cols_needed / cols
-    
+
     expanded_map = []
     for row in map_section:
         row = row * (int(multiplier) + 1)
@@ -25,20 +29,27 @@ def create_map(map_section):
     return expanded_map
 
 
-def solve_part_one(expanded_map):
-    position = 0
+def encounter_trees(expanded_map, slope):
+    x_position = 0
+    y_position = 0
+    move_right = slope[0]
+    move_down = slope[1]
+
     trees_encountered = 0
-
-    for row in expanded_map:
-        if row[position] == '#':
+    while len(expanded_map) > y_position:
+        if expanded_map[y_position][x_position] == '#':
             trees_encountered += 1
-        position = position + 3
+        x_position = x_position + move_right
+        y_position = y_position + move_down
 
-    return trees_encountered
+    slope.append(trees_encountered)
 
 
-def solve_part_two():
-    pass
+def multiply_trees(slopes):
+    result = 1
+    for slope in slopes:
+        result = result * slope[2]
+    return result
 
 
 if __name__ == "__main__":
