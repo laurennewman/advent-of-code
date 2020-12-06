66aa035ad7bbd1d2caa4f42199b1a2fde28b7acc
Lauren Newman <lanewman@redhat.com>
2020-12-06 16:00:45 -0500

    solved day 2 part 2

--- 2020/02/README.md
+++ 2020/02/README.md
@@ -1,11 +1,11 @@
 # [Advent of Code 2020 - Day 2](https://adventofcode.com/2020/day/2)
 
 ## Part 1
-How many passwords are valid?
+How many passwords are valid according to their policies?
 
 Solution: 625
 
 ## Part 2
+How many passwords are valid according to the new interpretation of the policies?
 
-
-Solution:
+Solution: 391
--- 2020/02/password-philosophy.py
+++ 2020/02/password-philosophy.py
@@ -3,34 +3,57 @@ def password_philosophy():
     with open('input') as f:
         passwords = f.read().splitlines()
 
-    part_one = None
-    part_two = None
+    valid_passwords_part_one = []
+    valid_passwords_part_two = []
 
-    valid_passwords = solve_part_one(passwords)
+    split_passwords = split(passwords)
 
-    print(f'Part 1: {valid_passwords} valid passwords')
+    for split_password in split_passwords:
+        rule_one = split_password[0]
+        rule_two = split_password[1]
+        letter = split_password[2]
+        password = split_password[3]
+
+        solve_part_one(rule_one, rule_two, letter, password, valid_passwords_part_one)
+        solve_part_two(rule_one, rule_two, letter, password, valid_passwords_part_two)
+
+    print(f'Part 1: {len(valid_passwords_part_one)} valid passwords')
+    print(f'Part 2: {len(valid_passwords_part_two)} valid passwords')
 
 
-def solve_part_one(passwords):
-    valid_passwords = []
+def split(passwords):
+    split_passwords = []
     for password in passwords:
         policy, password = password.split(': ')[0], password.split(': ')[1]
         required_frequency, letter = policy.split(' ')[0], policy[-1]
-        range_min, range_max = int(required_frequency.split('-')[0]), int(required_frequency.split('-')[1])
+        rule_one, rule_two = int(required_frequency.split('-')[0]), int(required_frequency.split('-')[1])
+        split_passwords.append([rule_one, rule_two, letter, password])
 
-        actual_frequency = 0
-        for char in password:
-            if char is letter:
-                actual_frequency += 1
-
-        if actual_frequency in range(range_min, range_max + 1):
-            valid_passwords.append(password)
-
-    return len(valid_passwords)
+    return split_passwords
 
 
-def solve_part_two():
-    pass
+def solve_part_one(rule_one, rule_two, letter, password, valid_passwords_part_one):
+    actual_frequency = 0
+    for char in password:
+        if char is letter:
+            actual_frequency += 1
+
+    if actual_frequency in range(rule_one, rule_two + 1):
+        valid_passwords_part_one.append(password)
+
+
+def solve_part_two(rule_one, rule_two, letter, password, valid_passwords_part_two):
+    rule_one = rule_one - 1
+    rule_two = rule_two - 1
+
+    if password[rule_one] is letter and password[rule_two] is letter:
+        pass
+    elif password[rule_one] is letter and password[rule_two] is not letter:
+        valid_passwords_part_two.append(password)
+    elif password[rule_one] is not letter and password[rule_two] is letter:
+        valid_passwords_part_two.append(password)
+    else:
+        pass
 
 
 if __name__ == "__main__":
